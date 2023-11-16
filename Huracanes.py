71f0a89d3eba7a876c112e7dd1b9a97a6f2183de
Luc0709 <145040211+Luc0709@users.noreply.github.com>
2023-11-16 12:02:48 -0400

    Add files via upload

---
+++ Huracanes.py
@@ -0,0 +1,19 @@
+wind =int(0)
+wind =int(input("Estudio de huracanes"))
+
+mhp= int(wind)
+
+elif <= 74 <=95:
+    print("esto se categoriza como un huracan categoria 1, siendo los daños minimos y puede llegar de 4-5 pies")  
+
+elif <= 96 <=110:
+    print("esto se categoriza como un huracan categoria 2, siendo los daños moderados y puede llegar de 6-8 pies") 
+
+ elif <= 111 <= 130:
+    print("esto se categoriza como un huracan categoria 3, siendo los daños extensos y puede llegar de 9-12 pies") 
+
+ elif <= 131 <=154:
+print("esto se categoriza como un huracan categoria 4, siendo los daños extremos y puede llegar de 13-28 pies")
+
+ elif <= 155:
+    print("esto se categoriza como un huracan categoria 6, siendo los daños catastroficos y puede llegar de 19 pies en adelante")             
